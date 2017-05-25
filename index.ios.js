af7a205a017831b2863662dacec9498497e27a42
sspgit <sunsuping@drupem.com>
2017-05-25 16:16:07 +0800

    设置宽高，弹性Flex

--- index.ios.js
+++ index.ios.js
@@ -112,10 +112,28 @@ color: 'red',
 	},
 
 });
-
+// 尺寸 弹性Flex 宽高
+// <View style={{width: 50,height:50,backgroundColor:'powderblue'}}/>
+			// <Text>I am  text </Text>
+			// <View style={{width:100,height:100,backgroundColor:'skyblue'}}/>
+			// <View style={{width:150,height:150,backgroundColor:'steelblue'}}/>
+class FixedDimensionsBasics extends Component{
+	render(){
+		return (
+			// 试试去掉父View中的`flex: 1`。
+      // 则父View不再具有尺寸，因此子组件也无法再撑开。
+      // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+			<View style={{flex: 1}}>
+			<View style={{flex:1,backgroundColor:'powderblue'}}/>
+			<View style={{flex:2,backgroundColor:'skyblue'}}/>
+			<View style={{flex:3,backgroundColor:'steelblue'}}/>
+			</View>
+			);
+	}
+}
 
 
 
 
 // 注意，这里用引号括起来的'AwesomeProject'必须和你init创建的项目名一致
-AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
+AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
