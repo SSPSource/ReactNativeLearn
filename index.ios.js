a5f3f90ef421eb99b6b975540cb460a6e065daaf
sspgit <sunsuping@drupem.com>
2017-05-25 10:54:56 +0800

    state状态学习--亮闪闪

--- index.ios.js
+++ index.ios.js
@@ -12,6 +12,8 @@ import {
   Image,
   View
 } from 'react-native';
+/* 假如我们需要制作一段不停闪烁的文字。文字内容本身在组件创建时就已经指定好了，所以文字内容应该是一个prop。而文字的显示或隐藏的状态（快速的显隐切换就产生了闪烁的效果）则是随着时间变化的，因此这一状态应该写到state中
+*/
 class HelloWorldApp extends Component{
   render(){
   return(
@@ -35,8 +37,55 @@ uri:'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
 			);
 	}
 }
+/*state 例子*/
 
+class Blink extends Component{
+	constructor(props){
+		super(props);
+		this.state = {showText: true };
+setInterval(() => {
+	this.setState(previousState => {
+		return { showText : !previousState.showText }
+	});
+}
+	,1000);
+	}
+	render(){ 
+		let display = this.state.showText ? this.props.text : 'null';
+		return (
+			<Text>{display}</Text>
+			);
+	}
+}
 
+class BlinkApp extends Component{
+	render(){
+		return(
+			<View>
+			<Blink text='I love to blink'/>
+			<Blink text='Yes Blinking is so great'/>
+			<Blink text='why did they ever take this out of HTML'/>
+			<Blink text='Look at me look at me look at me'/>
+			</View>
+			);
+	}
+}
+
+class point{
+	constructor(x,y){
+this.x=x;
+this.y=y;
+	}
+	toString(){
+		let str='{' + this.x + ',' + this.y + '}';
+		console.log(str);
+		// return ;
+	}
+}
+var b = new point();
+b.x=3;
+b.y=4;
+b.toString();
 
 // 注意，这里用引号括起来的'AwesomeProject'必须和你init创建的项目名一致
-AppRegistry.registerComponent('AwesomeProject', () => Bananas);
+AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
